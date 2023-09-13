fa800abc29d5b7c62a4fa2139c17dbf74ad19fba
Galih Setyawan <galih.kamvax032@gmail.com>
2023-09-13 09:25:47 +0700

    add sequelize setup & enhance route

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express, { Express, Request, Response } from "express";
-import commonRoute from "./module/common";
-
-const app: Express = express();
-const PORT: number = 4001;
-
-app.use("/", commonRoute);
-app.listen(PORT, () => console.log(`server is serving at : ${PORT}`));
---
+++ src/routes/index.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { UserController } from "../controllers/user";
+
+export default class MainRouter {
+  router: Router;
+  userController: UserController;
+
+  constructor() {
+    // Initialize controllers objects
+    this.userController = new UserController();
+
+    // Initialize router object
+    this.router = Router({ mergeParams: true });
+    this.userRoutes();
+  }
+
+  private userRoutes() {
+    this.router.get("/", (req, res) => {
+      res.json({
+        message: "Welcome to the API",
+      });
+    });
+
+    this.router
+      .route("/users")
+      .get(this.userController.readAll)
+      .post(this.userController.create);
+
+    this.router
+      .route("/users/:id")
+      .get(this.userController.read)
+      .put(this.userController.update)
+      .delete(this.userController.delete);
+  }
+}
